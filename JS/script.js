23678f05281ce8bed065d7ac7e8249caedd8b570
fernando <fernandobennoah@gmail.com>
2024-10-27 01:36:23 -0300

    Primeiro Commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+//
+const menuhamburguer = document.querySelector(".menu-hamburguer");
+menuhamburguer.addEventListener("click", () => {
+  toggleMenu();
+});
+
+function toggleMenu() {
+  const nav = document.querySelector(".nav-responsive");
+  menuhamburguer.classList.toggle("change");
+
+  if (menuhamburguer.classList.contains("change")) {
+    nav.style.display = "block";
+  } else {
+    nav.style.display = "none";
+  }
+}
